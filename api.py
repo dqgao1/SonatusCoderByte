56358ed9c379134b5619c9fe1b1b56a45e087efd
dqgao1 <dqgao1@gmail.com>
2025-04-03 17:33:48 -0400

    Added threading to check for expired logs
    Created multithreaded check to remove any expired logs and checks every minute to see if any logs are older than 1 hr

--- api.py
+++ api.py
@@ -2,15 +2,40 @@ from flask import Flask, jsonify, request
 import subprocess
 from datetime import datetime
 import time
+import threading
 
 app = Flask(__name__)
 
 logs = []
 
+EXPIRATION =  3600 # set time to remove logs (1hr)
+#EXPIRATION = 10 # set time to remove logs(1m) TEST case
 
+def remove_expired_logs():
+    while True:
+        current_time = time.time()
+        print(f"{current_time} Running checks for expired logs")
+        global logs
+        new_logs = []
+        for log in logs:
+            #print((log['timestamp'].replace("Z","").replace("T"," "))).timestamp()
+            datetime_obj = datetime.strptime(log['timestamp'], '%Y-%m-%d %H:%M:%S')
+            timestamp = datetime_obj.timestamp()
+            #log_timestamp = float(log['timestamp'].replace(":","").replace("Z","").replace("T"," "))
+            if current_time - timestamp < EXPIRATION:
+                new_logs.append(log)
+            else:
+                print("Log expired")
+
+        logs = new_logs
+        time.sleep(60)
+        
+
+threading.Thread(target=remove_expired_logs, daemon=True).start()
 #https://medium.com/@asvinjangid.kumar/creating-your-own-api-in-python-a-beginners-guide-59f4dd18d301
 @app.route('/logs', methods=['GET'])
 def get_logs():
+    #remove_expired_logs()
     return jsonify({"logs": logs})
 
 @app.route('/logs', methods=['POST'])
@@ -21,7 +46,7 @@ def post_logs():
         return jsonify({"error": "No JSON received"}), 400
     
     service_name = new_log.get('service_name', '?')
-    timestamp = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     message = new_log.get('message', '?')
 
     log_entry = {
