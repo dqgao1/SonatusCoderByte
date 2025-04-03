cde68abd724c1b2ff71749a4c8126c9ce6f20062
dqgao1 <dqgao1@gmail.com>
2025-04-03 19:40:13 -0400

    Removed some comments and added descriptions to api.py
    Added more descriptions to api.py
    
    *IMPORTANT
    Added ability to unsort keys so that response shows the same as the problem description E.g. timestamp then message instead of message then timestamp

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ import threading
 
 
 app = Flask(__name__)
+app.json.sort_keys = False # doesn't sort the json collection, without this message was first before timestamp (Constraint: Handling Unordered Logs)
 
 logs = []
 
@@ -19,10 +20,8 @@ def remove_expired_logs():
         global logs
         new_logs = []
         for log in logs:
-            #print((log['timestamp'].replace("Z","").replace("T"," "))).timestamp()
             datetime_obj = datetime.strptime(log['timestamp'].replace("T"," ").replace("Z",""), '%Y-%m-%d %H:%M:%S')
             timestamp = datetime_obj.timestamp()
-            #log_timestamp = float(log['timestamp'].replace(":","").replace("Z","").replace("T"," "))
             if current_time - timestamp < EXPIRATION:
                 new_logs.append(log)
             else:
@@ -31,11 +30,13 @@ def remove_expired_logs():
         logs = new_logs
         time.sleep(60)
         
-
+#Constraint: Thread-safee design
 threading.Thread(target=remove_expired_logs, daemon=True).start()
 #https://medium.com/@asvinjangid.kumar/creating-your-own-api-in-python-a-beginners-guide-59f4dd18d301
+#Constraint: Efficient Storage and Retrieval/Concurrency
 @app.route('/logs', methods=['GET'])
 def get_logs():
+    """GET /logs method, reads in arguments passed in either through the URL or cURL command, updates the values, filters them out based on start and end and returns a list of filtered out logs"""
     #remove_expired_logs()
     service_name = request.args.get('service_name')
     start_timestamp = request.args.get('start')
@@ -43,23 +44,14 @@ def get_logs():
 
     filtered_logs = []
 
-    #print(logs['timestamp'])
+    #Constraint: Memory Efficiency
     for log in logs:
         datetime_obj = datetime.strptime(log['timestamp'].replace("T"," ").replace("Z",""), '%Y-%m-%d %H:%M:%S')
         timestamp = datetime_obj.timestamp()
 
         start_timestamp_conversion = datetime.strptime(start_timestamp.replace("T", " ").replace("Z", ""), '%Y-%m-%d %H:%M:%S').timestamp()
         end_timestamp_conversion = datetime.strptime(end_timestamp.replace("T", " ").replace("Z", ""), '%Y-%m-%d %H:%M:%S').timestamp()
-        #print(start_timestamp_conversion)
-        #end_timestamp_conversion = 
 
-        print(timestamp)
-
-        print(f"{start_timestamp_conversion} start_timestamp")
-        print(f"{timestamp} current_timestamp")
-        print(f"{end_timestamp_conversion} end_timestamp")
-
-        print("")
         if (not service_name or log['service_name'] == service_name) and (not start_timestamp_conversion or timestamp >= start_timestamp_conversion) and (not end_timestamp_conversion or timestamp <= end_timestamp_conversion):
             filtered_logs.append({
                 "timestamp": log['timestamp'],
@@ -73,6 +65,7 @@ def get_logs():
 
 @app.route('/logs', methods=['POST'])
 def post_logs():
+    """POST /logs method, takes in service_name, timestamp and message and returns it as a JSON object"""
     new_log = request.get_json()
 
     if not new_log:
